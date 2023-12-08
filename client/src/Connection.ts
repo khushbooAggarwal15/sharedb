62bcf3ab9eb3cefc08b70f86acd9ad27232d2925
khushboo <aggarwal.khushi1501@gmail.com>
2023-12-08 22:31:49 +0530

    Comm

--- client/src/Connection.ts
+++ client/src/Connection.ts
@@ -4,7 +4,9 @@ import * as richText from "rich-text";
 
 sharedb.types.register(richText.type);
 
-var socket = new ReconnectingWebSocket("ws://localhost:3001") as any;
+var socket = new ReconnectingWebSocket(
+  "https://sharedb-backend.vercel.app/"
+) as any;
 
 var connection = new sharedb.Connection(socket);
 export default connection;
